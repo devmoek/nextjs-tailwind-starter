f03be00938416f2855a6b4703019a1b6136c27ac
Michael Myers <moek@devmoek.com>
2023-07-23 14:44:51 +0400

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main className="h-screen flex flex-col justify-center items-center">
+      <div className="text-left p-4 absolute top-0 left-0">
+        <a className="text-blue-600" href="https://github.com/devmoek">
+          moek
+        </a>
+      </div>
+      <div className="text-center">
+        <h1 className="text-5xl">Next.js Starter</h1>
+      </div>
+    </main>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  darkMode: "false",
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      }, 
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
